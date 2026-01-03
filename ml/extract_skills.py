a70268a4ecf32a5205c78749d7ce9ed5b57926f7
Pradeep <kp9177443280@gmail.com>
2026-01-03 14:11:06 +0530

    Fix spaCy model installation for Render

--- ml/extract_skills.py
+++ ml/extract_skills.py
@@ -1,5 +1,12 @@
 import spacy
-nlp = spacy.load("en_core_web_sm")
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 SKILLS_DB = [
     "python", "java", "javascript", "react", "node", "express",
